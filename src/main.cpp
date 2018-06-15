df75c1b7807efff78bc5d501d303fb149003f25c
Philipp Lutz <philipp.lutz@gmx.de>
2018-06-15 23:59:25 +0200

    Use eigen3 as system dependency

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include "Eigen-3.3/Eigen/Core"
-#include "Eigen-3.3/Eigen/QR"
+#include <Eigen/Core>
+#include <Eigen/QR>
 #include "json.hpp"
 
 using namespace std;
